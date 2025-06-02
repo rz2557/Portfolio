6e31be943d6af576297cc54d182b70d23d97121a
Rishan <rz2557@nyu.edu>
2025-06-02 20:53:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.getElementById("contactForm").addEventListener("submit", function(event
   event.preventDefault();
   alert("Thank you for your message. I will get back to you soon!");
 });
+
