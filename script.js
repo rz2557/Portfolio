3a489579371d0bfd2c777acc29a80d715858c43d
Rishan <rz2557@nyu.edu>
2025-06-02 19:56:08 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Thank you for your message. I will get back to you soon!");
+});
