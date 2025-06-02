3479f00746ce81b9e5b22daceb7d3fbe8f6a3971
Rishan <rz2557@nyu.edu>
2025-06-02 20:26:03 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 document.getElementById("contactForm").addEventListener("submit", function(event) {
   event.preventDefault();
   alert("Thank you for your message. I will get back to you soon!");
